d00ac4f0dc4c216831647fbaff39bf2ac7e578c4
barungh <barungh@hotmail.com>
2024-11-12 18:40:18 +0530

    mobile navbar all done

--- src/app/layout/mobilenav/mobilenav.component.ts
+++ src/app/layout/mobilenav/mobilenav.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mobilenav',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './mobilenav.component.html',
   styleUrl: './mobilenav.component.css'
 })
 export class MobilenavComponent {
+  isMenuOpen = signal(false);
+  showSlidingNav = signal(false);
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+    this.showSlidingNav.set(scrollPosition > 105);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen.update(value => !value);
+  }
 }
